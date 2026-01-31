6b6f9feeeee156793cec0fa2d18680fa8e780578
kihoon <nike6736@naver.com>
2026-01-31 12:05:28 +0900

    ✨ feat(#4): urls 설정

--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,7 @@ from drf_spectacular.views import (
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("admin/", admin.site.urls),
+    path("api/v1/user", include("apps.user.urls")),
 ]
 
 if settings.DEBUG:
