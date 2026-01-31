8ebc8b0ca15c6e6bf079ae645df51c93b53d4dbb
kihoon <nike6736@naver.com>
2026-01-31 17:35:43 +0900

    캐시

--- apps/post/serializers/post_create.py
+++ apps/post/serializers/post_create.py
@@ -7,8 +7,9 @@ class PostCreateSerializer(serializers.ModelSerializer):
     tags = serializers.ListField(
         child=serializers.CharField(max_length=50),
         required=False,  # 태그는 없을 수도 있음
-        write_only=True  # 저장할 때만 사용 (응답에는 포함 X)
+        write_only=True,  # 저장할 때만 사용 (응답에는 포함 X)
     )
+
     class Meta:
         model = Post
         fields = [
