22d3c9f8fd86fa4f286c833bb87ae2a705dd1e86
kihoon <nike6736@naver.com>
2026-01-31 16:55:12 +0900

    ✨ feat(#114): 포스트 등록 api 구현

---
+++ apps/post/serializers/post_create.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    tags = serializers.ListField(
+        child=serializers.CharField(max_length=50),
+        required=False,  # 태그는 없을 수도 있음
+        write_only=True  # 저장할 때만 사용 (응답에는 포함 X)
+    )
+    class Meta:
+        fields = [
+            "title",
+            "content",
+            "thumbnail",
+            "tags",
+        ]
---
+++ apps/post/views/post_api.py
@@ -0,0 +1,34 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from typing import cast
+
+from apps.post.services.post_create_service import create_post
+from apps.user.models import User
+from apps.post.serializers.post_create import PostCreateSerializer
+
+
+class PostAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request):
+        # 1. 입력 데이터 검증
+        serializer = PostCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # 2. 유저 타입 캐스팅 (Type Hinting)
+        user = cast(User, request.user)
+
+        # 3. 서비스레이어 호출
+        post = create_post(
+            author=user,
+            validated_data=serializer.validated_data,
+        )
+
+        # 4. 응답 반환
+        return Response(
+            {"id": post.id, "message": "질문이 성공적으로 등록되었습니다."},
+            status=status.HTTP_201_CREATED,
+        )
