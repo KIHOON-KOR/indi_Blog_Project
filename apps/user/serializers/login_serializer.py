2e85fc39e7578cf79d745e01a5b5951b5a7aac05
kihoon <nike6736@naver.com>
2026-02-15 13:46:45 +0900

    💡 chore(#4): black

--- apps/user/serializers/login_serializer.py
+++ apps/user/serializers/login_serializer.py
@@ -4,7 +4,5 @@ from rest_framework import serializers
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField(write_only=True, required=True)
     password = serializers.CharField(
-        write_only=True,
-        required=True,
-        style={"input_type": "password"}
+        write_only=True, required=True, style={"input_type": "password"}
     )
