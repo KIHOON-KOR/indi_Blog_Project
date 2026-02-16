1e7c7bf37ed546588857f4dd4d13c884e1aea954
kihoon <nike6736@naver.com>
2026-02-16 14:03:36 +0900

    ✨ feat(#): 에러메세지 관리 기능 적용

--- apps/user/serializers/signup_serializer.py
+++ apps/user/serializers/signup_serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from apps.core.exceptions.base import BaseCustomException
+from apps.core.exceptions.messages import ErrorMessage
 
 User = get_user_model()
 
@@ -20,11 +22,8 @@ class SignupSerializer(serializers.ModelSerializer):
     def validate_email(self, value):
         """
         이메일 중복 여부를 검증합니다.
-        Model의 unique=True가 있지만, 여기서 명시적인 에러 메시지를 주는 것이 UX에 좋습니다.
         """
         # 해당 이메일로 가입된 유저가 있는지 DB에서 확인
         if User.objects.filter(email=value).exists():
-            # 이미 존재한다면 유효성 검사 에러(400)를 발생
-            raise serializers.ValidationError("이미 존재하는 이메일입니다.")
-        # 문제가 없다면 입력받은 이메일 값을 그대로 반환
+            raise BaseCustomException(ErrorMessage.EMAIL_ALREADY_EXISTS)
         return value
