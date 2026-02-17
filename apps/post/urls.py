c1f952eaa1b59e9f293f7f81c3278a5f939648ba
kihoon <nike6736@naver.com>
2026-02-17 13:13:08 +0900

    ✨ feat(#15): 게시글 작성 디자인 추가 및 기능 연결

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from apps.post.views.page_views import post_write_page
 from apps.post.views.post_api import PostAPIView
 
 
 urlpatterns = [
     path("", PostAPIView.as_view(), name="post_create"),
+    path("write/", post_write_page, name="post_write_page")
 ]
