cb08597c200383016a6c5adb14b31ea9a782fb4d
kihoon <nike6736@naver.com>
2026-02-17 14:22:07 +0900

    ✨ feat(#17): 시리얼라이저 수정

--- apps/post/serializers/post_list.py
+++ apps/post/serializers/post_list.py
@@ -4,12 +4,13 @@ from apps.post.models import Post
 
 class PostListSerializer(serializers.ModelSerializer):
     """목록 조회를 위한 시리얼라이저"""
-
+    author_nickname = serializers.CharField(source='user.nickname', read_only=True)
     class Meta:
         model = Post
         fields = [
             "id",
             "title",
             "thumbnail",
+            "author_nickname",
             "created_at",
         ]
