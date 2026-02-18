7f9c45ff7eea7765a339e5168a742835fb68da6b
kihoon <nike6736@naver.com>
2026-02-18 11:45:50 +0900

    ✨ feat(#21): 구조 분리 및 서비스 고도화

--- apps/post/services/post_manage_service.py
+++ apps/post/services/post_manage_service.py
@@ -1,18 +1,28 @@
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
+
+from apps.core.exceptions.base import BaseCustomException
 from apps.post.models import Post
 from apps.user.models import User
-
+from apps.core.exceptions.messages import ErrorMessage
 
 def restore_temp_post(post_id: int, user: User):
     """임시글을 공개글로 전환(복구)합니다."""
-    post = get_object_or_404(Post, id=post_id, user=user, is_temp=True)
+    post = Post.objects.filter(id=post_id, user=user, is_temp=True).first()
+
+    if not post:
+        raise BaseCustomException(ErrorMessage.POST_NOT_FOUND)
+
     post.is_temp = False
-    post.save()
+    post.save(update_fields=['is_temp'])
 
 
 def soft_delete_post(post_id: int, user: User):
     """게시글을 삭제(Soft Delete) 처리합니다."""
-    post = get_object_or_404(Post, id=post_id, user=user, deleted_at__isnull=True)
+    post = Post.objects.filter(id=post_id, user=user, deleted_at__isnull=True).first()
+
+    if not post:
+        raise BaseCustomException(ErrorMessage.POST_NOT_FOUND)
+
     post.deleted_at = timezone.now()
-    post.save()
+    post.save(update_fields=['deleted_at'])
