81fd4a32a5ea6bfdc2305f78831615b335be4157
kihoon <nike6736@naver.com>
2026-02-19 15:05:33 +0900

    ✨ feat(#24): 공개 권한 추가

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -13,13 +13,16 @@ from apps.post.views.temp_post_api import MyTempManageAPIView, MyTempAPIView
 urlpatterns = [
     # [전체 피드 및 생성]
     path("", PostAPIView.as_view(), name="post_list_create"),
+
     # [내 블로그]
     path("my/", MyPostAPIView.as_view(), name="post_my_list"),
+
     # [임시글 관리]
     path("my/temp/", MyTempAPIView.as_view(), name="post_temp_list"),
     path(
         "my/temp/<int:post_id>/", MyTempManageAPIView.as_view(), name="post_temp_manage"
     ),
+
     # [화면(UI)]
     path("write/", post_write_page, name="post_write_page"),
     path("my/page/", my_post_list_page, name="my_post_list_page"),
