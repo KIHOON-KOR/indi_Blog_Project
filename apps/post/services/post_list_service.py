2fc5a6d11cbbcb976583e7283d25df0663b37d51
kihoon <nike6736@naver.com>
2026-02-22 19:34:55 +0900

    ✨ feat(#35): 좋아요기능 디자인 추가

--- apps/post/services/post_list_service.py
+++ apps/post/services/post_list_service.py
@@ -31,6 +31,7 @@ def get_my_published_posts(*, user: User) -> QuerySet[Post]:
             deleted_at__isnull=True,  # 삭제되지 않은 글만 필터링합니다.
         )
         .select_related("user")
+        .annotate(likes_count=Count('likes', distinct=True))
         .order_by("-created_at")
     )  # 성능을 위해 유저 정보를 미리 가져옵니다.
 
