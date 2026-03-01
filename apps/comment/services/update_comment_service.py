d98b61cb82299a2d9a0009295824dffd03f5dc39
kihoon <nike6736@naver.com>
2026-03-01 17:58:24 +0900

    ✨ feat(#41): 댓글 수정기능 구현

---
+++ apps/comment/services/update_comment_service.py
@@ -0,0 +1,29 @@
+from typing import Any
+from apps.user.models import User
+from django.db import transaction
+from apps.core.exceptions.messages import ErrorMessage
+from apps.comment.models import Comment
+from apps.core.exceptions.base import BaseCustomException
+
+
+@transaction.atomic
+def update_comment(
+    *, comment_id: int, user: User, validated_data: dict[str, Any]
+) -> Comment:
+    # 1. 대상 댓글이 존재하는지 검증
+    comment = Comment.objects.filter(id=comment_id).first()
+
+    # 2. 권한 검증(댓글 존재여부)
+    if not comment:
+        raise BaseCustomException(ErrorMessage.COMMENT_NOT_FOUND)
+
+    # 3. 권한 검증(댓글 작성자 여부)
+    if comment.user != user:
+        raise BaseCustomException(ErrorMessage.NOT_COMMENT_AUTHOR)
+
+    # 4. 데이터 업데이트
+    if "content" in validated_data:
+        comment.content = validated_data["content"]
+        comment.save(update_fields=["content"])
+
+    return comment  # type: ignore
