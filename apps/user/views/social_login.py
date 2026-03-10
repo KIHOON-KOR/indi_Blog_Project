955b401a51b40ddfc987ade9f01e8ad7249219e6
kihoon <nike6736@naver.com>
2026-03-10 11:09:02 +0900

    💡 chore(#64): black.

--- apps/user/views/social_login.py
+++ apps/user/views/social_login.py
@@ -2,7 +2,10 @@ from django.shortcuts import redirect
 from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from apps.user.services.social_login_service import GithubLoginService, DiscordLoginService
+from apps.user.services.social_login_service import (
+    GithubLoginService,
+    DiscordLoginService,
+)
 from rest_framework.response import Response
 from rest_framework import status
 
