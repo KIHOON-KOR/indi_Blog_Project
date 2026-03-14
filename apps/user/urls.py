e5488c8efb470be48cc8988e9fd03f61509d557c
kihoon <nike6736@naver.com>
2026-03-14 17:04:34 +0900

    ✨ feat(#80): 마이페이지 완성 - 2

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.user.views.login import LoginAPIView
-from apps.user.views.page_views import login_page, signup_page
+from apps.user.views.page_views import login_page, signup_page, mypage_view
 from apps.user.views.profile_view import UserProfileAPIView
 from apps.user.views.signup import SignupAPIView
 from apps.user.views.social_login import (
@@ -17,9 +17,13 @@ urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
     path("signup/", SignupAPIView.as_view(), name="signup"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
     # 브라우저 접속용
     path("login-page/", login_page, name="login_page"),
     path("signup-page/", signup_page, name="signup_page"),
+    path("mypage/", mypage_view, name="mypage_page"),
+
+
     # 소셜로그인(GitHub)
     path("login/github/", GithubLoginAPIView.as_view(), name="github_login"),
     path(
--- apps/user/views/page_views.py
+++ apps/user/views/page_views.py
@@ -9,3 +9,8 @@ def login_page(request):
 def signup_page(request):
     """회원가입 화면(HTML)을 렌더링합니다."""
     return render(request, "user/signup.html")
+
+
+def mypage_view(request):
+    """마이페이지 화면(HTML)을 렌더링합니다."""
+    return render(request, "user/mypage.html")
